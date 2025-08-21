5d2f776f973ee306006c2719aad1cc85074b4ca4
Jay <jaybhardwaj17601@gmail.com>
2025-08-21 06:17:01 +0530

    db dont print

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   console.log("connecting to database...")
   try {
-    console.log(`Mongo URL: ${process.env.MONGO_URI}`);
+    // console.log(`Mongo URL: ${process.env.MONGO_URI}`);
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       // useNewUrlParser: true,
       // useUnifiedTopology: true,
